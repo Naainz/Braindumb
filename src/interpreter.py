624f44a228eea952f2321455fbd6cbe748b50279
Stefan <67121265+Naainz@users.noreply.github.com>
2024-08-20 20:57:36 +0300

    add ironman logic

--- src/interpreter.py
+++ src/interpreter.py
@@ -8,6 +8,7 @@ class Interpreter:
         self.tokens = self.lexer.tokenize()
         self.variables = {}
         self.erased_values = set()
+        self.restored_values = set()
 
     def run(self):
         while self.tokens:
@@ -21,7 +22,7 @@ class Interpreter:
 
         if token.type == "IDENTIFIER":
             if token.value == "i":
-                self._handle_inevitable()
+                self._handle_inevitable_or_ironman()
             elif token.value == "print!":
                 self._handle_print()
             elif token.value == "red":
@@ -29,7 +30,7 @@ class Interpreter:
             else:
                 self._handle_assignment(token)
 
-    def _handle_inevitable(self):
+    def _handle_inevitable_or_ironman(self):
         if self.tokens and self.tokens[0].value == "am":
             self.tokens.pop(0)
             if self.tokens and self.tokens[0].value == "inevitable":
@@ -39,8 +40,29 @@ class Interpreter:
                     if value_token.type == "STRING":
                         stripped_value = value_token.value.strip("'\"")
                         self.erased_values.add(stripped_value)
+                        if stripped_value in self.restored_values:
+                            self.restored_values.remove(stripped_value)
                     elif value_token.type == "NUMBER":
-                        self.erased_values.add(str(value_token.value))  # Store as string for comparison
+                        self.erased_values.add(str(value_token.value))
+                        if str(value_token.value) in self.restored_values:
+                            self.restored_values.remove(str(value_token.value))
+            elif self.tokens and self.tokens[0].value == "ironman":
+                self.tokens.pop(0)
+                if self.tokens:
+                    value_token = self.tokens.pop(0)
+                    if value_token.value == "*":
+                        self.restored_values.update(self.erased_values)
+                        self.erased_values.clear()
+                    elif value_token.type == "STRING":
+                        stripped_value = value_token.value.strip("'\"")
+                        if stripped_value in self.erased_values:
+                            self.restored_values.add(stripped_value)
+                            self.erased_values.remove(stripped_value)
+                    elif value_token.type == "NUMBER":
+                        value_str = str(value_token.value)
+                        if value_str in self.erased_values:
+                            self.restored_values.add(value_str)
+                            self.erased_values.remove(value_str)
 
     def _handle_print(self):
         if self.tokens:
@@ -49,7 +71,8 @@ class Interpreter:
                 result = str(self.variables[value_token.value])
                 for word in self.erased_values:
                     result = result.replace(word, "")
-                print(result.strip())
+                if result.strip() and result not in self.erased_values:
+                    print(result.strip())
 
     def _handle_red_variable(self):
         if self.tokens:
